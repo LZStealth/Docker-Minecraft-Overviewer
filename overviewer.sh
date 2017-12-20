3a2d330eccf2e08acb12558d51054ddc944aa33f
Mark Rapson <markrapson.1991@gmail.com>
2017-12-20 12:53:07 +0000

    Update overviewer.sh

--- overviewer.sh
+++ overviewer.sh
@@ -1,4 +1,6 @@
-overviewer.py --config=/minecraft/overviewer.cfg &&
+#!/bin/bash
+
+overviewer.py --config=/minecraft/overviewer.cfg > /proc/1/fd/1 2> /proc/1/fd/2 &&
 echo "Land Pass Complete" &&
-overviewer.py --config=/minecraft/overviewer.cfg --genpoi &&
+overviewer.py --config=/minecraft/overviewer.cfg --genpoi > /proc/1/fd/1 2> /proc/1/fd/2 &&
 echo "POI Pass Complete"
