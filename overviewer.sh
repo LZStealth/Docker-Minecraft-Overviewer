b461b067924388ba3a1326c6c67cb07c475e67ed
Mark Rapson <markrapson.1991@gmail.com>
2017-12-20 13:30:14 +0000

    Delete overviewer.sh

--- overviewer.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-overviewer.py --config=/minecraft/overviewer.cfg > /proc/1/fd/1 2> /proc/1/fd/2 &&
-echo "Land Pass Complete" &&
-overviewer.py --config=/minecraft/overviewer.cfg --genpoi > /proc/1/fd/1 2> /proc/1/fd/2 &&
-echo "POI Pass Complete"
