f2e171c0ca2b999f8e33dc7839368d5cef436bce
Mark Rapson <markrapson.1991@gmail.com>
2017-12-21 18:52:00 +0000

    Update start-overviewer.sh

--- start-overviewer.sh
+++ start-overviewer.sh
@@ -1,1 +1,5 @@
-exec $COMMAND
+if [ "$RENDER" == true ]; then
+overviewer.py --config=/minecraft/overviewer.cfg
+
+if [ "$POI" == true ]; then
+overviewer.py --config=/minecraft/overviewer.cfg --genpoi
