b28e3e49198390034bc4ba85c8153a36b2df032f
Mark Rapson <markrapson.1991@gmail.com>
2018-11-04 11:57:13 +0000

    Update start-overviewer.sh

--- start-overviewer.sh
+++ start-overviewer.sh
@@ -1,9 +1,9 @@
 if [ "$RENDER" == true ]; then
-  overviewer.py --config=/minecraft/overviewer.cfg
+  python overviewer.py --config=/minecraft/overviewer.cfg
 fi
 
 if [ "$POI" == true ]; then
-  overviewer.py --config=/minecraft/overviewer.cfg --genpoi
+  python overviewer.py --config=/minecraft/overviewer.cfg --genpoi
 fi
 
 chown root:www-data -R /mcmap 
