3aec5e423593aa9fe3a2448c7d16f615c3d9c723
Mark Rapson <markrapson.1991@gmail.com>
2019-06-28 17:48:26 +0100

    Update start-overviewer.sh

--- start-overviewer.sh
+++ start-overviewer.sh
@@ -6,4 +6,5 @@ if [ "$POI" == true ]; then
   python3 overviewer.py --config=/minecraft/overviewer.cfg --genpoi
 fi
 
-chown root:www-data -R /mcmap 
+chown root:www-data -R /mcmap
+chmod 755 -R /mcmap/*
