a0c2f479a8f018e63ce1486d317d16acc6138950
Mark Rapson <markrapson.1991@gmail.com>
2020-01-04 11:06:21 +0000

    Added ForceRender

--- start-overviewer.sh
+++ start-overviewer.sh
@@ -6,5 +6,9 @@ if [ "$POI" == true ]; then
   python3 overviewer.py --config=/minecraft/overviewer.cfg --genpoi
 fi
 
+if [ "$FORCERENDER" == true ]; then
+  python3 overviewer.py --config=/minecraft/overviewer.cfg --forcerender
+fi
+
 chown root:www-data -R /mcmap
 chmod 755 -R /mcmap/*
