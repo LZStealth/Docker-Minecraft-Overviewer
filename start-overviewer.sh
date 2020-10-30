bac210695f920c835533d4d30eec7d72cb1bdd7b
Mark Rapson <markrapson.1991@gmail.com>
2020-10-30 09:24:31 +0000

    Update start-overviewer.sh

--- start-overviewer.sh
+++ start-overviewer.sh
@@ -1,3 +1,17 @@
+# Require MINECRAFT_VERSION environment variable to be set (no default assumed)
+if [ -z "$MINECRAFT_VERSION" ]; then
+  echo "Expecting environment variable MINECRAFT_VERSION to be set to non-empty string. Exiting."
+  exit 1
+fi
+
+if [ -f "/versions/${MINECRAFT_VERSION}.jar" ]; then
+    echo "Minecraft client ${MINECRAFT_VERSION}.jar exists! Skipping download."
+else
+    CLIENT_URL=$(python3 /mcVersionGet.py "${MINECRAFT_VERSION}")
+    echo "Using Client URL ${CLIENT_URL} to download ${MINECRAFT_VERSION}.jar."
+    wget "${CLIENT_URL}" -O "/versions/${MINECRAFT_VERSION}.jar"
+fi
+
 if [ "$RENDER" == true ]; then
   python3 overviewer.py --config=/minecraft/overviewer.cfg
 fi
